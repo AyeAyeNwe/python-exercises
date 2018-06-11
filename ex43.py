8fd8b3a379c814d743f98bfae9a8511ba91884d2
AyeAyeNwe <aan172406@gmail.com>
2018-06-11 22:57:29 +0630

    Exercise43

--- ex43.py
+++ ex43.py
@@ -1,3 +1,29 @@
+class Scene(object):
+    def enter(self):
+        print("This scene is not yet configured.")
+        print("Subclass it and implement enter().")
+        exit(1)
+class Engine(object):
+    def__init__(self, scene_map):
+        self.scene_map = scene_map
+    def play(self):
+        current_scene = self.scene_map.opening_scene()
+        last_secne = self.scene_map.next_scene('finished')
+        while current_scene != last_scene:
+            next_scene_name = current_scene.enter()
+            current_scene = self.scene_map.next_scene(next_scene_name_
+        current_scene.enter()
+class Death(Scene):
+    quips = [
+            "You died. You kinda suck at this.",
+            "Your Mom would be proud ... if she were smarter.",
+            "such a luser",
+            I have a small puppy that's better at this."'
+            You're worse than your Dad's jokes."
+            ]
+    def enter(self):
+        print(Death.quips[randit(0, len (self.quips)-10])
+        exit(1)
 class CentralCorridor(Scene):
     def enter(self):
         print(dedent("""
@@ -49,4 +75,18 @@ class CentralCorridor(Scene):
         else:
             print("Does Not Compute!")
             return 'central_corridor'
+class LaserWeaponArmory(Scene):
+    def enter(self):
+        print(dedent("""
+                YOu do a dive roll into the Weapon Armory,crouch and scan
+                the room for more Gothons that might be hiding. It's dead 
+                quiet ,too quiet.YOu stand up and run to the far side of
+                the room and find the neutron bomb in its container.
+                There's a keypad lock on the box and you need the code to 
+                get the bomb out.If you get the code wrong 10 times then
+                thelock closes forever and you can't get the bomb.The 
+                code is 3 digits."""))
+        
+
+
        
