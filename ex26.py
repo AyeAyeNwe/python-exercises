77c389c6ccc6ebb49d2bd1b23dc797f78b2ce003
AyeAyeNwe <aan172406@gmail.com>
2018-06-15 10:36:57 +0630

    Exercise26

--- ex26.py
+++ ex26.py
@@ -5,15 +5,22 @@ print("How tall are you?", end='')
 height = input()
 print("How much do you weigh?", end='')
 weight = input()
+
 print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+from sys import argv
 script, filename = argv
+
 txt = open(filename)
+
 print("Here's your file {filename}:")
 print(txt.read())
+
 print("Type the filename again:")
 file_again = input("> ")
-test.txt = open(file_again)
-print(test.txt_read())
+
+txt = open(file_again)
+print(txt_again.read())
+
 print("let's practice everything.")
 print("You\'d need to know \'about escapes with  \\ that do \n newlines and \t tabs.")
 
@@ -25,40 +32,53 @@ nor comprehend passion frkom intuition
 and requires an explanation 
 \n\t\twhere there is none.
 """
+
 print("----------")
 print(poem)
 print("-----------")
+
 five = 10 - 2 + 3 - 6
 print(f"This should be five:{five}")
+
 def secret_formula(started):
     jelly_beans = started * 500
     jars = jelly_beans / 1000
     crates = jars * 100
     return jelly_beans, jars, crates
+
 start_point = 10000
 beans, jars = secret_formula(start_point)
 print("With a starting point of: {}".format(start_point))
+
 print(f"WE'd have {beans} beans, {jars} jars, and {crates} crates.")
 start_point = start_point / 10
 print("We can also do that this way:")
 formula = secret_formula(startpoint)
 print("We'd have {} beans,{} jars, and {} crates.".format(*formula))
+
 people = 20
 cats = 30 
 dogs = 15
+
 if people < cats:
     print("Too many cats! The world is doomed!")
+
 if people < cats:
     print("Not many cats!The world is saved!")
+
 if people < dogs:
     print("The world is drooled on!")
+
 if people > dogs:
     print("The world is dry!")
+
 dogs += 5
 if people >= dogs:
     print("People are greater than or equal to dogs.")
+
 if people <= dogs:
     print("People are less than or equal to dogs.")
+
 if people == dogs:
     print("People are dogs.")
 
