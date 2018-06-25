fabfd5047751f7212b9029ed7eaf3fdfecc844a8
AyeAyeNwe <aan172406@gmail.com>
2018-06-25 21:48:22 +0630

    Updated exercise 4

--- ex4.py
+++ ex4.py
@@ -1,14 +1,15 @@
-cars=100
-space-in-a-car=4.0
-drivers=30
-passengers=90
-cars-not-driven=cars-drivers
-cars-drivern=drivers
-carpool-capacity=cars-driven*space-in-a-car
-avereage-passenger-per-car=passengers/cars-driver
-print("There are ",cars,"cars available")
-print("There are only ",drivers,"drivers available")
-print("There will be ", cars-not-driven,"empty-cars-today")
-print("We can transport",carpool-capacity,"people")
-print("We need to put about",average-passengers-per-car, "in each car.")
+cars = 100
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
+cars_not_driven = cars - drivers
+cars_drivers = drivers
+carpool_capacity = cars_drivers * space_in_a_car
+average_passengers_per_car = passengers / cars-drivers
+print("There are ", cars, "cars available")
+print("There are only ", drivers, "drivers available")
+print("There will be ", cars_not_driven, "empty cars today")
+print("We can transport", carpool_capacity, "people today")
+print("WE have", passengers, "to carpool today")
+print("We need to put about", average_passengers_per_car, "in each car.")
 
