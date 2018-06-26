c00ae83c336442d0cd43d094f7fb18601719485c
AyeAyeNwe <39288331+AyeAyeNwe@users.noreply.github.com>
2018-06-26 09:59:56 +0630

    Updated Changed Rat to Pet

--- ex101.py
+++ ex101.py
@@ -2,7 +2,7 @@ tabby_pet = "\t I'm tabbed in."
 persian_pet = "I'm split \non a line ."
 backslash_pet = "I'm \\ a \\ pet."
 
-fat_t=pet"""
+fat_pet = """
 I'll do a list:
  \t* Pet Food
 \t* Fishes
